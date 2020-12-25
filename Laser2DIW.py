fe19c217c21cfe1d9856c28a091c5f529fa1c580
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2020-12-24 19:47:20 -0800

    comments and adjustments

--- Laser2DIW.py
+++ Laser2DIW.py
@@ -10,7 +10,6 @@ prime = [['5', '5', '0'], ['200', '5', '0'],  # coordinates of high pressure pri
          ['200', '10', '0'], ['5', '10', '0']]  # coordinates of nominal pressure prime line
 prime_f = '2400'  # G1 F value for prime lines
 
-
 ##########################################
 # initialize remaining values
 ##########################################
@@ -42,6 +41,7 @@ def dist(x, y, z, x2, y2, z2):  # 3d euclidean distance
 for line in f:  # parses through line by line
     if ('G1 ' in line) or ('G0 ' in line):  # how to process movement commands
 
+        # Update the target location
         X = line[line.find('X') + 1:
                  line.find(' ', line.find('X'))]
         Y = line[line.find('Y') + 1:
@@ -49,6 +49,8 @@ for line in f:  # parses through line by line
         Z = line[line.find('Z') + 1:
                  line.find(' ', line.find('Z'))]
 
+        # if there is no new info on X Y or Z data, use previous data
+        # check for 'G' because one will appear if line.find is unsuccessful
         if 'G' in X:
             X = mem_X
         else:
@@ -62,44 +64,44 @@ for line in f:  # parses through line by line
         else:
             Z = float(Z)
 
-        distance = dist(X, Y, Z, mem_X, mem_Y, mem_Z)
-
-        E_value += distance * flow_rate
-
-        mem_X = float(X)
-        mem_Y = float(Y)
-        mem_Z = float(Z)
-
         if 'ON' in mem_line:  # convert S command to E A B
+            distance = dist(X, Y, Z, mem_X, mem_Y, mem_Z)
+            E_value += distance * flow_rate
             S_value = min(max(int(mem_line.replace('ON S', '')) +
                               ratio_calib, 0), 255)
-
             AB_ratio = ' E' + str(round(E_value, 3)) + \
                        ' A' + str(round(1 - (S_value / 255), 3)) + \
                        ' B' + str(round(S_value / 255, 3))
-
             print(line.strip() + AB_ratio)
 
         elif 'OFF' in mem_line:  # pure movement (no extrusion)
             print(line.strip() + mem_line.replace('OFF', ''))
+        # remember current position
+        mem_X = float(X)
+        mem_Y = float(Y)
+        mem_Z = float(Z)
 
     elif 'G90' in mem_line:  # G90 appears right before the main print
         print('G1' + prime_f)
         for num in range(2):  # repeats twice, once per prime line
             n = num * 2
+            #  get in position to start line
             print('G0 X' + prime[n][0] + ' Y' + prime[n][1] + ' Z' + prime[n][2])
             mem_X = float(prime[n][0])
             mem_Y = float(prime[n][1])
             mem_Z = float(prime[n][2])
-            distance = dist(float(prime[n+1][0]), float(prime[n+1][1]),
-                            float(prime[n+1][2]), mem_X, mem_Y, mem_Z)
-            if num == 0:
+            distance = dist(float(prime[n + 1][0]), float(prime[n + 1][1]),
+                            float(prime[n + 1][2]), mem_X, mem_Y, mem_Z)
+            if num == 0:  # sets a different flow rate for first prime line
                 E_value += distance * flow_rate_p
             else:
                 E_value += distance * flow_rate
-            print('G1 X' + prime[n+1][0] + ' Y' + prime[n+1][1] + ' E' +
+            # extrude the line
+            print('G1 X' + prime[n + 1][0] + ' Y' + prime[n + 1][1] + ' E' +
                   str(E_value) + ' A0.5 B0.5')
 
+            # after the high-pressure line, the E_value needs to be reset
+            # because the stepper motors have skipped steps
             if num == 0:
                 E_value = 0
                 print("G92 E0")
@@ -108,13 +110,13 @@ for line in f:  # parses through line by line
             ('ON' not in line) and ('G0' not in line):  # everything else
         print(line)
 
+    # the line is remembered to enable combining two lines into one
     mem_line = line
 
-
 ##########################################
-# Add end code
+# add end code
 ##########################################
-E_value = max(0, E_value-10)
+E_value = max(0, E_value - 10)
 print('G0 Z10 E' + str(round(E_value, 3)))
 print('G0 X0 Y0')
 
