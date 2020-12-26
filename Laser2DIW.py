656ec184da029d893a23f45b0e6d717f228c5660
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2020-12-26 13:56:58 -0800

    comments and formatting

--- Laser2DIW.py
+++ Laser2DIW.py
@@ -8,10 +8,10 @@ from tkinter import filedialog
 flow_rate = .01  # flow rate
 flow_rate_p = .1  # high-pressure prime flow rate
 ratio_calib = 0  # adjusts ratio to favor A (negative) or B (positive) -255:255
-prime = [['5', '5', '0'], ['200', '5', '0'],  # coordinates of high pressure prime line
-         ['200', '10', '0'], ['5', '10', '0']]  # coordinates of nominal pressure prime line
+prime = [['5', '5', '0'], ['200', '5', '0'],  # high pressure prime line xyz
+         ['200', '10', '0'], ['5', '10', '0']]  # nominal pressure prime line xyz
 prime_f = '2400'  # G1 F value for prime lines
-depressurization_extruder_distance = 10  # positive value
+depressurization_extruder_distance = 10  # positive value, applied at end of print
 
 ##########################################
 # initialize remaining values
@@ -24,17 +24,15 @@ mem_Z = 0
 
 ##########################################
 # open original file
+# saveas new file
 ##########################################
-
 root = tk.Tk()
 root.withdraw()
-
-open_file_path = filedialog.askopenfilename(title="Select original G-code file to convert")
-
+open_file_path = filedialog.askopenfilename(title="Select original G-code file"
+                                                  " to convert")
 f = open(open_file_path, 'r')
-
-save_file_path = filedialog.asksaveasfilename(title="SaveAs new converted G-code file")
-
+save_file_path = filedialog.asksaveasfilename(title="SaveAs new converted G-code"
+                                                    " file")
 s = open(save_file_path + '.gcode', 'x+')
 
 
@@ -91,6 +89,7 @@ for line in f:  # parses through line by line
         elif 'OFF' in mem_line:  # pure movement (no extrusion)
             print(line.strip() + mem_line.replace('OFF', ''))
             s.write(line.strip() + mem_line.replace('OFF', '') + '\n')
+
         # remember current position
         mem_X = float(X)
         mem_Y = float(Y)
@@ -99,20 +98,24 @@ for line in f:  # parses through line by line
     elif 'G90' in mem_line:  # G90 appears right before the main print
         print('G1 ' + prime_f)  # manually sets feed rate
         s.write('G1 ' + prime_f + '\n')
+
         for num in range(2):  # repeats twice, once per prime line
             n = num * 2
             #  get in position to start line
             print('G0 X' + prime[n][0] + ' Y' + prime[n][1] + ' Z' + prime[n][2])
-            s.write('G0 X' + prime[n][0] + ' Y' + prime[n][1] + ' Z' + prime[n][2] + '\n')
+            s.write('G0 X' + prime[n][0] + ' Y' + prime[n][1] +
+                    ' Z' + prime[n][2] + '\n')
             mem_X = float(prime[n][0])
             mem_Y = float(prime[n][1])
             mem_Z = float(prime[n][2])
             distance = dist(float(prime[n + 1][0]), float(prime[n + 1][1]),
                             float(prime[n + 1][2]), mem_X, mem_Y, mem_Z)
+
             if num == 0:  # sets a different flow rate for first prime line
                 E_value += distance * flow_rate_p
             else:
                 E_value += distance * flow_rate
+
             # extrude the line
             print('G1 X' + prime[n + 1][0] + ' Y' + prime[n + 1][1] + ' E' +
                   str(E_value) + ' A0.5 B0.5')
